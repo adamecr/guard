0ef0133ae704f0bc75cc89a9ef1edcfaeb688076
Radek Adamec <radek@adamec.net>
2019-01-03 00:33:05 +0100

    JUST THE PoC - the Soft<T> check is quite resource heavy Checks the argument using the checkFunction. When the check fails, the correctionFunction is applied to argument with optional re-check. When the re-check fails, the exception is thrown.

---
+++ src/Guard.Soft.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Dawn
+{
+    //JUST THE PoC - the Soft<T> check is quite resource heavy
+    public partial class Guard
+    {
+        /// <summary>
+        ///     Checks the argument using the <paramref name="checkFunction"/>.
+        ///     When the check fails, the <paramref name="correctionFunction"/> is applied to argument
+        ///     with optional re-check. When the re-check fails, the exception is thrown.
+        /// <para>
+        ///    JUST THE PoC - the <see cref="Soft{T}"/> check is quite resource heavy
+        /// </para>
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type that the argument's value should be an instance of.
+        /// </typeparam>
+        /// <param name="argument">The object argument.</param>
+        /// <param name="checkFunction">
+        /// Argument check function (may be a chain of checks).
+        /// The check function must keep the argument type.
+        /// </param>
+        /// <param name="correctionFunction">Argument correction function.</param>
+        /// <param name="message">Error message used when the correction function fails </param>
+        /// <param name="recheck">Flag whether the re-check the corrected argument</param>
+        /// <returns>A new <see cref="ArgumentInfo{T}" />.</returns>
+        /// <exception cref="Exception">
+        ///     <paramref name="argument" /> value is not an instance of type <typeparamref name="T" />.
+        /// </exception>
+        [DebuggerStepThrough]
+        public static ArgumentInfo<T> Soft<T>(
+            in this ArgumentInfo<T> argument,
+            Func<ArgumentInfo<T>, ArgumentInfo<T>> checkFunction,
+            Func<ArgumentInfo<T>, T> correctionFunction,
+            Func<T, string> message = null,
+            bool recheck = false)
+
+        {
+            try
+            {
+                //Check and if OK return the result
+                return checkFunction(argument);
+            }
+            catch (Exception)
+            {
+                ArgumentInfo<T> retVal;
+                try
+                {
+                    //Try to correct
+                    retVal = argument.Modify(correctionFunction(argument));
+                }
+                catch (Exception ex)
+                {
+                    //Can't correct/modify
+                    var m = message?.Invoke(argument.Value) ?? Messages.Require(argument);
+                    throw argument.Exception(new ArgumentException(m, argument.Name, ex));
+                }
+                //Recheck if needed (without additional correction)
+                return !recheck ? retVal : checkFunction(retVal);
+            }
+        }
+    }
+}
---
+++ tests/SoftTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Xunit;
+
+namespace Dawn.Tests
+{
+    public class SoftTests:BaseTests
+    {
+        [Theory(DisplayName = "Empty correction test")]
+        [InlineData("A")]
+        public void EmptyCorr(string argument)
+        {
+           var v=Guard.Argument(argument).Soft(a => a.Empty(), b => "c");
+           Assert.Equal("c",v);
+            
+        }
+
+        [Theory(DisplayName = "Empty OK test")]
+        [InlineData("")]
+        public void EmptyOk(string argument)
+        {
+            var v = Guard.Argument(argument).Soft(a => a.Empty(), b => "c");
+            Assert.Equal("", v);
+
+        }
+
+        [Theory(DisplayName = "Min OK test")]
+        [InlineData(6)]
+        public void MinOk(int argument)
+        {
+            var v = Guard.Argument(argument).Soft(a => a.Min(5), b => 5);
+            Assert.Equal(6, v);
+
+        }
+
+        [Theory(DisplayName = "Min correction test")]
+        [InlineData(4)]
+        public void MinCorr(int argument)
+        {
+            var v = Guard.Argument(argument).Soft(a => a.Min(5), b => 5);
+            Assert.Equal(5, v);
+
+        }
+
+        [Theory(DisplayName = "Min+Max correction test (correct min)")]
+        [InlineData(4)]
+        public void MinMaxCorrectionWithMin(int argument)
+        {
+            var v = Guard.Argument(argument).Soft(a => a.Min(5).Max(10), b => b<5?5:10);
+            Assert.Equal(5, v);
+
+        }
+        [Theory(DisplayName = "Min+Max OK test")]
+        [InlineData(8)]
+        public void MinMaxCorrectionOk(int argument)
+        {
+            var v = Guard.Argument(argument).Soft(a => a.Min(5).Max(10), b => b < 5 ? 5 : 10);
+            Assert.Equal(8, v);
+
+        }
+        [Theory(DisplayName = "Min+Max correction test (correct max)")]
+        [InlineData(15)]
+        public void MinMaxCorrectionWithMax(int argument)
+        {
+            var v = Guard.Argument(argument).Soft(a => a.Min(5).Max(10), b => b < 5 ? 5 : 10);
+            Assert.Equal(10, v);
+
+        }
+
+        [Theory(DisplayName = "Recheck OK test")]
+        [InlineData(4)]
+        public void RecheckOk(int argument)
+        {
+            var v = Guard.Argument(argument).Soft(a => a.Min(5), b => 5,recheck:true);
+            Assert.Equal(5, v);
+
+        }
+
+        [Theory(DisplayName = "Recheck fail test")]
+        [InlineData(4)]
+        public void RecheckFail(int argument)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                Guard.Argument(argument).Soft(a => a.Min(5), b => 4, recheck: true));
+        }
+
+        [Theory(DisplayName = "Exception in correction func test")]
+        [InlineData(4)]
+        public void CorrectionException(int argument)
+        {
+            var exception = Assert.Throws<ArgumentException>(() =>
+                Guard.Argument(argument).Soft(
+                    a => a.Min(5),
+                    a => throw new Exception(),
+                    (a)=>"ModifyErr"));
+            Assert.StartsWith("ModifyErr", exception.Message);
+
+        }
+    }
+}
